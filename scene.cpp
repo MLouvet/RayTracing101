96f5b08bf63a3f7a1abe3189eadcca00112386a6
MLouvet <louvetm@hotmail.fr>
2017-11-26 14:16:39 +0100

    Normalized normal vector of sphere, implemented diffusion of light

--- scene.cpp
+++ scene.cpp
@@ -14,80 +14,95 @@
 //  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
 //
 
+#include <algorithm>
 #include "scene.h"
 #include "material.h"
 
 Color Scene::trace(const Ray &ray)
 {
-    // Find hit object and distance
-    Hit min_hit(std::numeric_limits<double>::infinity(),Vector());
-    Object *obj = NULL;
-    for (unsigned int i = 0; i < objects.size(); ++i) {
-        Hit hit(objects[i]->intersect(ray));
-        if (hit.t<min_hit.t) {
-            min_hit = hit;
-            obj = objects[i];
-        }
-    }
+	// Find hit object and distance
+	Hit min_hit(std::numeric_limits<double>::infinity(), Vector());
+	Object *obj = NULL;
+	for (unsigned int i = 0; i < objects.size(); ++i) {
+		Hit hit(objects[i]->intersect(ray));
+		if (hit.t < min_hit.t) {
+			min_hit = hit;
+			obj = objects[i];
+		}
+	}
 
-    // No hit? Return background color.
-    if (!obj) return Color(0.0, 0.0, 0.0);
+	// No hit? Return background color.
+	if (!obj) return Color(0.0, 0.0, 0.0);
 
-    Material *material = obj->material;            //the hit objects material
-    Point hit = ray.at(min_hit.t);                 //the hit point
-    Vector N = min_hit.N;                          //the normal at hit point
-    Vector V = -ray.D;                             //the view vector
+	Material *material = obj->material;            //the hit objects material
+	Point hit = ray.at(min_hit.t);                 //the hit point
+	Vector N = min_hit.N;                          //the normal at hit point
+	Vector V = -ray.D;                             //the view vector
 
 
-    /****************************************************
-    * This is where you should insert the color
-    * calculation (Phong model).
-    *
-    * Given: material, hit, N, V, lights[]
-    * Sought: color
-    *
-    * Hints: (see triple.h)
-    *        Triple.dot(Vector) dot product
-    *        Vector+Vector      vector sum
-    *        Vector-Vector      vector difference
-    *        Point-Point        yields vector
-    *        Vector.normalize() normalizes vector, returns length
-    *        double*Color        scales each color component (r,g,b)
-    *        Color*Color        dito
-    *        pow(a,b)           a to the power of b
-    ****************************************************/
+	/****************************************************
+	* This is where you should insert the color
+	* calculation (Phong model).
+	*
+	* Given: material, hit, N, V, lights[]
+	* Sought: color
+	*
+	* Hints: (see triple.h)
+	*        Triple.dot(Vector) dot product
+	*        Vector+Vector      vector sum
+	*        Vector-Vector      vector difference
+	*        Point-Point        yields vector
+	*        Vector.normalize() normalizes vector, returns length
+	*        double*Color        scales each color component (r,g,b)
+	*        Color*Color        dito
+	*        pow(a,b)           a to the power of b
+	****************************************************/
 
-    Color color = material->color;                  // place holder
+	Color cAmbiant, cDiffuse, cSpecular;                  // place holder
+	cAmbiant = cDiffuse = cSpecular = Color(0, 0, 0);
 
-    return color;
+	for (unsigned int i = 0; i < lights.size(); i++) {
+		Light* l = lights[i];
+		Vector vL = (l->position - hit).normalized();
+
+		//Calculation of ambient light - TODO
+
+		//Calculation of diffuse reflection
+		cDiffuse = material->kd * material->color * max(vL.dot(N), 0.0);
+
+		//Calculation of specular light - TODO
+
+	}
+
+	return cAmbiant + cDiffuse + cSpecular;
 }
 
 void Scene::render(Image &img)
 {
-    int w = img.width();
-    int h = img.height();
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            Point pixel(x+0.5, h-1-y+0.5, 0);
-            Ray ray(eye, (pixel-eye).normalized());
-            Color col = trace(ray);
-            col.clamp();
-            img(x,y) = col;
-        }
-    }
+	int w = img.width();
+	int h = img.height();
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			Point pixel(x + 0.5, h - 1 - y + 0.5, 0);
+			Ray ray(eye, (pixel - eye).normalized());
+			Color col = trace(ray);
+			col.clamp();
+			img(x, y) = col;
+		}
+	}
 }
 
 void Scene::addObject(Object *o)
 {
-    objects.push_back(o);
+	objects.push_back(o);
 }
 
 void Scene::addLight(Light *l)
 {
-    lights.push_back(l);
+	lights.push_back(l);
 }
 
 void Scene::setEye(Triple e)
 {
-    eye = e;
+	eye = e;
 }
--- sphere.cpp
+++ sphere.cpp
@@ -48,15 +48,13 @@ Hit Sphere::intersect(const Ray &ray)
 
 	Point intersection;
 	//Calculating the discriminant
-	double a, b, c, d, t, ox, oy, oz; //ox.y.z just used for calculus
-	ox = ray.O.x - position.x;
-	oy = ray.O.y - position.y;
-	oz = ray.O.z - position.z;
+	double a, b, c, d, t;
+	Point distance = ray.O - position;
 
-	a = ray.D.x*ray.D.x + ray.D.y*ray.D.y + ray.D.z*ray.D.z;
-	b = 2 * (ray.D.x * ox + ray.D.y * oy + ray.D.z * oz);
-	c = ox*ox + oy*oy + oz*oz - r*r;
-	d = b*b - 4 * a*c;
+	a = ray.D.length_2();
+	b = 2 * ray.D.dot(distance);
+	c = distance.length_2() - pow(r, 2);
+	d = pow(b, 2) - 4 * a * c;
 
 	if (d < 0.0) {
 		return Hit::NO_HIT();
@@ -66,27 +64,15 @@ Hit Sphere::intersect(const Ray &ray)
 		t = -b / (2 * a);
 	}
 	else {
-		double t1, t2, x1, x2, y1, y2, z1, z2, l1, l2;
-		Point p1, p2;
+		double t1, t2;
 		//Two hits, at t = (-b - sqrt(d))/2a and (-b + sqrt(d))/2a
 		//Determining minimum distance between source of ray and collision
 		t1 = (-b - sqrt(d)) / (2 * a);
 		t2 = (-b + sqrt(d)) / (2 * a);
 
-		x1 = ray.D.x * t1 + ray.O.x;
-		y1 = ray.D.y * t1 + ray.O.y;
-		z1 = ray.D.z * t1 + ray.O.z;
-		x2 = ray.D.x * t2 + ray.O.x;
-		y2 = ray.D.y * t2 + ray.O.y;
-		z2 = ray.D.z * t2 + ray.O.z;
-
-		//Comparing square of lengths to get the nearest
-		l1 = (x1 - ray.O.x) * (x1 - ray.O.x) + (y1 - ray.O.y) * (y1 - ray.O.y) + (z1 - ray.O.z) * (z1 - ray.O.z);
-		l2 = (x2 - ray.O.x) * (x2 - ray.O.x) + (y2 - ray.O.y) * (y2 - ray.O.y) + (z2 - ray.O.z) * (z2 - ray.O.z);
-		t = l1 < l2 ? t1 : t2;
+		t = t1 < t2 ? t1 : t2;
 	}
 
-	intersection = Point::Triple(ray.D.x * t + ray.O.x, ray.D.y * t + ray.O.y, ray.D.z * t + ray.O.z);
 
 
 	/****************************************************
@@ -98,7 +84,7 @@ Hit Sphere::intersect(const Ray &ray)
 	* Insert calculation of the sphere's normal at the intersection point.
 	****************************************************/
 
-	Vector N = (intersection - position);
-
-	return Hit(t, N);
+	intersection = ray.D * t + ray.O;
+	Vector N = intersection - position;
+	return Hit(t, N.normalized());
 }
